050285a2378f78e81a53f570d50dfd04a4cfefb1
IT_SURABAYA_2025 <230082993+itsby25@users.noreply.github.com>
2025-10-16 14:29:29 +0700

    Update user.blade.php

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -32,6 +32,7 @@
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                     <div>
                     <form method="post" action="user_new">
+                     @csrf   
                     <h2>Form Registrasi User Baru</h2>
                     <hr>               
                         <div class="form-group">
